73fc33536e6e76d4d59e14d16ef0fabfe49e82a7
fangbinwei <fangbinwei@yeah.net>
2021-01-06 20:43:56 +0800

    fix: fix comment

--- app-use-corejs3/src/main.js
+++ app-use-corejs3/src/main.js
@@ -1,5 +1,5 @@
 // ie11
-// 1. @vue\babel-preset-app inject 'es6.promise' 'es7.promise.finally' from corejs2
+// 1. @vue\babel-preset-app inject 'es.promise' 'es.promise.finally' from corejs3
 
 // 2.The Promise polyfill injected by 'lib-with-promise-polyfill-corejs3' which doesn't contain 'finally'
 // overwrite the global Promise polyfilled by 'lib-with-promise-polyfill-corejs3'
