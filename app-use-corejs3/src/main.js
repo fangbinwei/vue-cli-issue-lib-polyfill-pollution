63021989392ea8eea4aabae0d78100d0beff7dd1
fangbinwei <fangbinwei@yeah.net>
2021-01-07 04:22:20 +0800

    fix: fix comment

--- app-use-corejs3/src/main.js
+++ app-use-corejs3/src/main.js
@@ -1,9 +1,10 @@
 // ie11
 // 1. @vue\babel-preset-app inject 'es.promise' 'es.promise.finally' from corejs3
-
 // 2.The Promise polyfill injected by 'lib-with-promise-polyfill-corejs3' which doesn't contain 'finally'
-// overwrite the global Promise polyfilled by 'lib-with-promise-polyfill-corejs3'
-import {output} from '../../lib-with-promise-polyfill-corejs3/dist/lib-with-promise-polyfill-corejs3.common.js'
+// overwrite the global Promise polyfilled by '@vue\babel-preset-app'
+
+// use !! to disable all loader (babel-loader, etc.)
+import {output} from '!!../../lib-with-promise-polyfill-corejs3/dist/lib-with-promise-polyfill-corejs3.common.js'
 
 // finally is not a function in ie11
 // no error if we comment 'import {output} ...' in ie11
